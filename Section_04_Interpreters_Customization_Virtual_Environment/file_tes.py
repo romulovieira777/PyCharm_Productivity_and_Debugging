91c5d3143514a37d63f4c12e7082746561a8b39f
Romulo Vieira <romulo.vieira777@gmail.com>
2022-12-13 20:45:38 -0300

    Interpreters Customization & Virtual Environment

--- Section_04_Interpreters_Customization_Virtual_Environment/file_tes.py
+++ Section_04_Interpreters_Customization_Virtual_Environment/file_tes.py
@@ -1,3 +1,39 @@
 import flask
+import mongo
+import math
 
-print('Hello World')
+print('Hello World Python')
+
+
+def prints():
+    print('Hello World')
+
+
+prints()
+
+
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+
+    print('Function end')
+
+
+def foo(bar):
+    bar = 1
+    return bar
+
+
+def main():
+    print(math.sqrt(4))
+    print('Hello World Pycharm')
+
+
+def func():
+    pass
+
+
+if __main__ == '__main__':
+    main()
