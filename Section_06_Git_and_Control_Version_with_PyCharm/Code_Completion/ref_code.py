d063046d0acdaee8a397900a2994902ea9c1b3a9
Romulo Vieira <romulo.vieira777@gmail.com>
2023-01-10 18:30:34 -0300

    Git and Control Version with PyCharm

---
+++ Section_06_Git_and_Control_Version_with_PyCharm/Code_Completion/ref_code.py
@@ -0,0 +1,7 @@
+from point import Point
+
+
+if __name__ == '__main__':
+    p1 = Point(1, 0)
+    p2 = Point(5, 3)
+    print(f'Distance between {p1} and {p2} is {p1.distance(p2)}')
---
+++ Section_06_Git_and_Control_Version_with_PyCharm/Code_Completion/ref_code_4.py
@@ -0,0 +1,6 @@
+from point import Point
+
+if __name__ == '__main__':
+    p1 = Point(3, 9)
+    p2 = Point(5, 12)
+    print(f'Distance between {p1} and {p2} is {p1.distance(p2)}')
