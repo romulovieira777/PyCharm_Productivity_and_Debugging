fb59097b04af6e83a93f01a1cfb8f5ac82a6fa4f
Romulo Vieira <romulo.vieira777@gmail.com>
2023-02-22 16:01:10 -0300

    Building a Web Application in PyCharm with Django

--- Section_09_Building_a_Web_Application_in_PyCharm_with_Django/django-project/gamestore/gamestore/settings.py
+++ Section_09_Building_a_Web_Application_in_PyCharm_with_Django/django-project/gamestore/gamestore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wkg^wbxxv!+up4vdrpxry2_5prh7+8l7y(-+=z3zhxfqk6n)i$"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
